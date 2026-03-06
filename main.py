1d52c27d0c8b5eb34c68c2f7e0c1d7ffc446ab1d
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-03-06 08:50:03 -0600

    feat: update main.py

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ def smallest_gap(s):
             letters_found_dic[s[pos]].append(pos)
         pos += 1
     s_pos = 0
-    
-    while s_pos < len(letters_found_dic[s[s_pos]]):
-        pos = 0
-        next_pos = 1
-        print(letters_found_dic[s[pos]])
-        if len(letters_found_dic[s[pos]])>1:
-            if letters_found_dic[s[pos]][next_pos] - letters_found_dic[s[pos]][pos] < diff:
-                diff = letters_found_dic[s[pos]][next_pos] - letters_found_dic[s[pos]][pos]
-                value1 = letters_found_dic[s[pos]][pos]
-                value2 = letters_found_dic[s[pos]][next_pos]
-            pos = next_pos
-            next_pos += 1
+    while s_pos < len(s):
+        if s[s_pos] in letters_found_dic.keys():
+            pos = 0
+            next_pos = 1
+            while next_pos < len(letters_found_dic[s[s_pos]])>1:
+                if letters_found_dic[s[s_pos]][next_pos] - letters_found_dic[s[s_pos]][pos] < diff:
+                    diff = letters_found_dic[s[s_pos]][next_pos] - letters_found_dic[s[s_pos]][pos]
+                    value1 = letters_found_dic[s[s_pos]][pos]
+                    value2 = letters_found_dic[s[s_pos]][next_pos]
+                elif (letters_found_dic[s[s_pos]][next_pos] - letters_found_dic[s[s_pos]][pos] == diff) and (value1>letters_found_dic[s[s_pos]][pos]):
+                    value1 = letters_found_dic[s[s_pos]][pos]
+                    value2 = letters_found_dic[s[s_pos]][next_pos]
+                pos = next_pos
+                next_pos += 1
+            del letters_found_dic[s[s_pos]]
         s_pos += 1
-    print(letters_found_dic)
     print(value1, value2)
     return s[value1+1:value2]
 
@@ -34,6 +36,6 @@ if __name__ == "__main__":
     print('-----')
     print(smallest_gap("A{5e^SD*F4i!o#q6e&rkf(po8|we9+kr-2!3}=4"))
     print('-----')
-    #print(smallest_gap("Hello World"))
+    print(smallest_gap("Hello World"))
     print('-----')
-    #print(smallest_gap("The quick brown fox jumps over the lazy dog."))
+    print(smallest_gap("The quick brown fox jumps over the lazy dog."))
